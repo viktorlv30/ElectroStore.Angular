7a2d3f7ef75c3fd0e094b6854bcb02a8bb6ad193
viktorlv30 <viktor.lv.30@gmail.com>
2016-11-04 16:23:56 +0200

    add folder header and profile

---
+++ app/app.config.js
@@ -0,0 +1,14 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when("/", {
+            templateUrl: "app/main.htm"
+        })
+        .when("/profile", {
+            templateUrl: "app/views/enterIntoDash.htm",
+            controller: "dashboardCtrlVitya"
+        })
+        .otherwise({ redirectTo: "/" });
+});
